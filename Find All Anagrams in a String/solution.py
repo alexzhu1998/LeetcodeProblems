cbb89b1cc223f99c25a4e0e48504949dcf34631e
alexzhu1998 <55743621+alexzhu1998@users.noreply.github.com>
2020-05-18 15:17:53 +1000

    update

--- Find All Anagrams in a String/solution.py
+++ Find All Anagrams in a String/solution.py
@@ -1,12 +1,13 @@
 import array 
 import bisect
 import collections 
-import sortedcontainers
+# import sortedcontainers
 
 class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
-
+    def findAnagrams(self, s: str, p: str):#List[int]
+        pass
 
 
 if __name__ == "__main__":
-    
+    f = open("input.txt", "r")
+    print(f)
