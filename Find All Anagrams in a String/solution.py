6bc216bf44b07d148b027e1701630b2c070e47f3
alexzhu1998 <55743621+alexzhu1998@users.noreply.github.com>
2020-05-18 15:56:49 +1000

    solution

--- Find All Anagrams in a String/solution.py
+++ Find All Anagrams in a String/solution.py
@@ -4,10 +4,46 @@ import collections
 # import sortedcontainers
 
 class Solution:
-    def findAnagrams(self, s: str, p: str):#List[int]
-        pass
+    def findAnagrams(self, s: str, p: str):# -> List[int]:
+        if s == "":
+            return []
+        l = []
+        
+        d = {}
+        dp = {}
+        for item in p:
+            if item in dp:
+                dp[item] += 1
+            else:
+                dp[item] = 1
+        for item in s[0:len(p)]:
+            if item in d:
+                d[item] += 1
+            else:
+                d[item] = 1
+        if d == dp:
+            l.append(0)
+        for i in range(1,len(s)-len(p)+1):
+            strL = s[i-1:len(p)+i]
+            
+            if d[strL[0]] > 1:
+                d[strL[0]] -= 1
+            else:
+                d.pop(strL[0],None)
+            
+                
+            if strL[-1] in d:
+                d[strL[-1]] += 1
+            else:
+                d[strL[-1]] = 1
+
+            if d == dp:
+                l.append(i)
+            
+        return l
 
 
 if __name__ == "__main__":
-    f = open("input.txt", "r")
-    print(f)
+    with open("input.txt", "r") as f:
+        f_contents = f.readline()
+        print(f_contents)
